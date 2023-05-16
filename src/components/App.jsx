72aaf8141f618c0fb3051f763c1e2e9f697aaeb2
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-16 15:14:32 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import React from 'react';
+import Container from './Container';
+import ContactForm from './ContactForm/ContactForm';
+import ContactsList from './ContactsList';
+import Filter from './Filter';
+import Loader from './Loader';
+import { MainTitle, SecondTitle } from './Container/Container.styled';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contacts/operations';
+import { selectError, selectIsLoading } from 'redux/contacts/selectors';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <MainTitle>Phonebook</MainTitle>
+      <ContactForm />
+      <SecondTitle>Contacts</SecondTitle>
+      <Filter />
+      {isLoading && !error && <Loader />}
+      <ContactsList />
+    </Container>
   );
 };
