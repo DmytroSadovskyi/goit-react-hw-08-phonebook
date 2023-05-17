baf63f6ab8c62d5b58d14cf0756d048ea4e67623
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-17 19:45:48 +0300

    Update Navigation.styled.jsx

--- src/components/Navigation/Navigation.styled.jsx
+++ src/components/Navigation/Navigation.styled.jsx
@@ -2,21 +2,21 @@ import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 export const Nav = styled.nav`
   display: flex;
-  gap: 5px;
+  gap: 7px;
 `;
 
 export const StyledLink = styled(NavLink)`
   display: inline-block;
   text-decoration: none;
   padding: 12px 0 12px 0;
-  font-size: 10px;
+  font-size: 14px;
   @media screen and (min-width: 420px) {
-    font-size: 16px;
+    font-size: 20px;
   }
   font-weight: 700;
-  color: white;
+  color: black;
 
   &.active {
-    color: black;
+    color: white;
   }
 `;
