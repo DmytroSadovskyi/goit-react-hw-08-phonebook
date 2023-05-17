c44ca1cdb6b626e967601714e3dfb276522be52f
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-17 19:21:44 +0300

    Update HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { MainTitle } from 'components/Container/Container.styled';
+const HomePage = () => {
+  return (
+    <main>
+      <section>
+        <MainTitle>This is your personal phone book</MainTitle>
+      </section>
+    </main>
+  );
+};
+
+export default HomePage;
