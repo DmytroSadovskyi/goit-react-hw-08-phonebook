940c467cc8dd6259ec7aa7a88ceaac282fa52166
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-17 20:13:28 +0300

    Update UserMenu.styled.jsx

--- src/components/UserMenu/UserMenu.styled.jsx
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -7,9 +7,9 @@ export const Wrapper = styled.div`
 `;
 
 export const UserName = styled.span`
-  font-size: 12px;
+  font-size: 10px;
   @media screen and (min-width: 420px) {
-    font-size: 17px;
+    font-size: 15px;
   }
   font-weight: 700;
 `;
