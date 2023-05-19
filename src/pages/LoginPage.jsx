c952636c14ed481724ecad0c095fcf7501cba5bc
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-19 18:43:02 +0300

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,25 @@
 import Container from 'components/Container/Container';
-
 import LoginForm from 'components/LoginForm/LoginForm';
-
+import { useSelector } from 'react-redux';
+import { selectError } from 'redux/auth/selectors';
 const LoginPage = () => {
+  const error = useSelector(selectError);
   return (
     <main>
       <section>
         <Container>
           <LoginForm />
+          {error && (
+            <p
+              style={{
+                color: 'red',
+                marginTop: '5px',
+                textAlign: 'center',
+              }}
+            >
+              Ooops, something went wrong... Please, try again
+            </p>
+          )}
         </Container>
       </section>
     </main>
