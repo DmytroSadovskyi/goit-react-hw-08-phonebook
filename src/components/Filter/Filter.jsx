eeaf2b992954ee08e78f05915e96f855ee2f5f2d
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-19 15:40:24 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,22 +1,23 @@
-import { nanoid } from 'nanoid';
-import { FilterLabel, FilterInput } from './Filter.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectFilter } from 'redux/contacts/selectors';
 import { updateFilter } from 'redux/contacts/filterSlice';
+import { TextField } from '@mui/material';
 
-const inputId = nanoid();
 const Filter = () => {
   const value = useSelector(selectFilter);
   const dispatch = useDispatch();
 
   return (
     <>
-      <FilterLabel htmlFor={inputId}>Find contacts by name</FilterLabel>
-      <FilterInput
-        type="text"
-        value={value}
-        id={inputId}
+      <TextField
+        label="Find contacts by name"
+        variant="outlined"
         onChange={e => dispatch(updateFilter(e.target.value))}
+        id="Find contacts by name"
+        value={value}
+        size="small"
+        sx={{ mb: 4 }}
+        InputLabelProps={{ shrink: true }}
       />
     </>
   );
