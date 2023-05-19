02076344b69fdd92208817c76871b9ef8fd96e8d
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-19 18:43:05 +0300

    Update RegisterPage.jsx

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,12 +1,20 @@
 import Container from 'components/Container/Container';
 import RegisterForm from 'components/RegisterForm/RegisterForm';
+import { useSelector } from 'react-redux';
+import { selectError } from 'redux/auth/selectors';
 
 const RegisterPage = () => {
+  const error = useSelector(selectError);
   return (
     <main>
       <section>
         <Container>
           <RegisterForm />
+          {error && (
+            <p style={{ color: 'red', marginTop: '5px', textAlign: 'center' }}>
+              Ooops, something went wrong... Please, try again
+            </p>
+          )}
         </Container>
       </section>
     </main>
